06a2b8d0d53f65201af8bf81da219e235570a341
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-25 19:28:34 +0200

    Only warn about SDCLANG exclusion once
    Please finally shut up. Thanks.
    
    Change-Id: If288e23d43a984a8397b9316eb9711aa3907f435

--- common.mk
+++ common.mk
@@ -15,9 +15,12 @@ BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
 include device/qcom/common/utils.mk
 
 ifneq ($(HOST_OS),linux)
-$(warning ****************************************************************)
-$(warning * SDCLANG is not supported on non-linux hosts. Disabling...)
-$(warning ****************************************************************)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
 else
 # include definitions for SDCLANG
 include device/qcom/common/sdclang/sdclang.mk
